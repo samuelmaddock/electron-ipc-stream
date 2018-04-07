a6dd3af79cfba196585607ac82506bfaddb0e7fc
Samuel Maddock <samuel.maddock@gmail.com>
2018-04-07 12:48:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var isRenderer = require('is-electron-renderer')
-
-if (isRenderer) {
-  module.exports = require('./src/rend')
-} else {
-  module.exports = require('./src/main')
-}
+module.exports = require('./src/main')
